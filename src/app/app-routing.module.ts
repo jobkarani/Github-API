6d86b7821dc6df01a0a37324c29213d0d051c0e7
job karani <job.nyambura@student.moringaschool.com>
2021-10-16 12:45:17 +0300

    creates navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profiles/profile.component';
+import { ReposComponent } from './repos/repos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'repos',component:ReposComponent},
+  {path:'profiles',component:ProfileComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ReposComponent } from './repos/repos.component';
+import { ProfileComponent } from './profiles/profile.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ReposComponent,
+    ProfileComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
