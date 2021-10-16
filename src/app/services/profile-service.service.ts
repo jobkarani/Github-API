8fdb2cae3179a66f444b2f0b61e33689cd62954f
job karani <job.nyambura@student.moringaschool.com>
2021-10-16 16:43:25 +0300

    creates new user and repos and assigns them empty values

--- src/app/services/profile-service.service.ts
+++ src/app/services/profile-service.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { User } from '../homeclasses/user'; 
+import { Repos } from '../homeclasses/repos'; 
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileServiceService {
+  userInfor!: User;
+  repositoryInfor!: Repos;
 
-  constructor() { }
+  constructor(private http:HttpClientModule) { 
+    this.userInfor= new User(
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      0,
+      0,
+      0,
+      new Date(),
+
+    ),
+    this.repositoryInfor = new Repos(
+      '',
+      '',
+      '',
+      new Date(),
+    )
+
+  }
 }
