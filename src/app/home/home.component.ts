ad3c77165fe458781478ce61e15ea5d9ce34f182
job karani <job.nyambura@student.moringaschool.com>
2021-10-17 17:53:36 +0300

    creates getUser and getRepos functions

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Repos } from '../homeclasses/repos';
+import { User } from '../homeclasses/user';
+import { ProfileServiceService } from '../services/profile-service.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+    user!: User;
+    repositories!: Repos;
+    
+  constructor(private request : ProfileServiceService ) {}
 
-  constructor() { }
+  getuser(gitUsername:any){
+    this.request.getUserDataRequest(gitUsername).then(
+      (response)=>{
+        this.user = this.request.userInfor;
+      },
+      (error:any) => {
+        console.log(error);
+      }
+    );
+  }
+
+  getRepos(gitUsername:any){
+    this.request.getReposRequest(gitUsername).then(
+      (response)=>{
+        this.repositories = this.request.repositoryInfor;
+      },
+      (error:any) => {
+        console.log(error);
+      }
+    );
+  }
 
   ngOnInit(): void {
+    this.getuser("jobkarani")
+    this.getRepos("jobkarani")
   }
 
 }
